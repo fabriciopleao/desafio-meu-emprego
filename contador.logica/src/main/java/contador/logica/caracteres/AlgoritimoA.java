9d3769fa14f1e71326f660030a0aaf3b1404d82e
Fabricio Leão <fabriciopereiraleao@gmail.com>
2024-03-06 11:44:33 -0300

    att código

--- contador.logica/src/main/java/contador/logica/caracteres/AlgoritimoA.java
+++ contador.logica/src/main/java/contador/logica/caracteres/AlgoritimoA.java
@@ -2,46 +2,45 @@ package contador.logica.caracteres;
 
 public class AlgoritimoA {
 
-	//FASE 1 - LOGICA DE PROGRAMAÇÃO
-	// Montar um algoritimo para ler a quantidade de letras 'a' na mensagem abaixo
-    //"Eu sou a pessoa mais incrível que eu posso ser, e através do meu esforço vou conquistar o que já é MEU."
-	
-	public static int contarLetra(String frase, char a) {
-		
-		//método tranformando todos os caracteres da frase desejada em minúsculas
-		frase = frase.toLowerCase();
-		
-		//variável indicando a inicialização da contagem
-		int contadorLetra = 0;
-		
-		//método transformando a letra desejada em mínuscula
-		char letraLowerCase = Character.toLowerCase(a);
+	// LOGICA DE PROGRAMAÇÃO
+	// Algoritimo criado para verificar a quantidade de vezes que um determinado
+	// caracterer aparece em uma mensagem.
 
-		//o loop percorre cada caractere na string 'frase'
-		for (int i = 0; i < frase.length(); i++) {
-			
-		//método para obter o caractere na posição 'i' da string 'frase'
-		//onde o caractere é armazenado na váriavel 'letterA'
-			char letterA = frase.charAt(i);
-			
-		//verificar se a letra 'a' é igual à letra convertida para minúsculas
-			if (letterA == letraLowerCase) {
-				
-		//Se o caractere na posição atual for igual à letra desejada
-		//o contador é incrementado, indicando que uma ocorrência da letra foi encontrada.		
+	public static int contarLetra(String mensagem, char caractereDesejado) {
+
+		// variável indicando a inicialização da contagem
+		int contadorLetra = 0;
+
+		// o loop percorre cada caractere na string 'frase'
+		for (int i = 0; i < mensagem.length(); i++) {
+
+			// método para obter o caractere na posição 'i' da string 'frase'
+			char caractere = mensagem.charAt(i);
+
+			// Verifica se o caractere atual é igual ao caractere desejado
+			if (caractere == caractereDesejado) {
+				// Incrementa o contador se for igual
 				contadorLetra++;
 			}
 		}
-		//retorna o valor atual do contador
+		// retorna o valor atual do contador
 		return contadorLetra;
 	}
-		//método main com finalidade de contar quantos caracteres 'a' existem na frase
-	public static void main(String[] args) {
-		String frase = "Eu sou a pessoa mais incrível que eu posso ser, e através do meu esforço vou conquistar o que já é MEU.";
-		char letraA = 'a';
-		int resultado = contarLetra(frase, letraA);
 
-		System.out.println("Número de vezes que a letra '" + letraA + "' aparece: " + resultado);
+	// método main com finalidade de contar quantos caracteres 'a' existem na frase
+	public static void main(String[] args) {
+
+		// informe o caractere
+		char caractere = 'a';
+		
+		// informe a mensagem
+		String mensagem = "Eu sou a pessoa mais incrível que eu posso ser, e através do meu esforço vou conquistar o que já é MEU.";
+
+		// informe a frase
+		int resultado = contarLetra(mensagem, caractere);
+
+		// imprime a quantidade
+		System.out.println(resultado);
 
 	}
 
